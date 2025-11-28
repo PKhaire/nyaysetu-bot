1fa72bb0a3a63fe2c0894e906a427d48f4399c8f
PKhaire <emptycupdevelopers@gmail.com>
2025-11-28 13:23:39 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,16 @@
+# db.py
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
