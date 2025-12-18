7b6598ed635dae19b9df09681a72c7f3409f6afd
PKhaire <emptycupdevelopers@gmail.com>
2025-12-18 12:38:23 +0530

    Create category_labels.py

---
+++ category_labels.py
@@ -0,0 +1,49 @@
+# category_labels.py
+
+CATEGORY_LABELS = {
+    "Family": {
+        "en": "Family",
+        "hi": "Family (Parivar)",
+        "mr": "कौटुंबिक",
+    },
+    "Criminal": {
+        "en": "Criminal",
+        "hi": "Criminal (Aparadh)",
+        "mr": "फौजदारी",
+    },
+    "Accident": {
+        "en": "Accident",
+        "hi": "Accident (Hadsa)",
+        "mr": "अपघात",
+    },
+    "Property": {
+        "en": "Property",
+        "hi": "Property (Jaaydad)",
+        "mr": "मालमत्ता",
+    },
+    "Business": {
+        "en": "Business",
+        "hi": "Business (Vyapar)",
+        "mr": "व्यवसाय",
+    },
+    "Job": {
+        "en": "Job",
+        "hi": "Job (Naukri)",
+        "mr": "नोकरी",
+    },
+    "Consumer": {
+        "en": "Consumer",
+        "hi": "Consumer (Grahak)",
+        "mr": "ग्राहक",
+    },
+    "Banking & Finance": {
+        "en": "Banking & Finance",
+        "hi": "Banking aur Finance",
+        "mr": "बँकिंग व आर्थिक",
+    },
+    "Other": {
+        "en": "Other",
+        "hi": "Other (Any)",
+        "mr": "इतर",
+    },
+}
